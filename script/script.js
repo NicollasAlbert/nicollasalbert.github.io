27477e70421aa3cb6f6c3eb75bc239332ed1d4b4
Nicollas Albert <nicksz55@hotmail.com>
2023-03-13 20:16:41 -0300

    Alterações

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 function initScrollSmooth() {
-  const linksInternos = document.querySelectorAll("a[href^='#']");
+  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
 
   function slideSmooth(e) {
+    e.preventDefault();
     const href = this.getAttribute("href");
     const section = document.querySelector(href);
 
